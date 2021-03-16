187fce78c5e9e1fbed47433d10ce8646f66f2dd6
Mgiar2015 <mgiar2015@gmail.com>
2021-03-16 17:52:04 -0400

    Added Cordinates to bottom of page

--- src/App.js
+++ src/App.js
@@ -2,31 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { Button, TextField, FormControl } from '@material-ui/core';
 import axios from 'axios';
 import Table from './components/Table';
-
-
+import { Grid } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
 
 
 function App() {
   const apiKey = "7083cd16161bdb6686c87eb80fd31922"
+  const [cordinates,setCordinates] = useState(["33.441792","-94.037689"])
   const [weatherData,setWeatherData] = useState(null);
   const [maxFilter,setMax] = useState(1000000);
   const [minFilter,setMin] = useState(-1000);
-  const cook = true
 
 
   const pullWeatherData = async() => {
     const lat = "33.441792";
     const long = "-94.037689";
-    const apiUrl = "https://api.openweathermap.org/data/2.5/onecall?exclude=hourly,minutely,alerts,current&lat="+lat+"&lon="+long+"&appid="+apiKey
+    const apiUrl = "https://api.openweathermap.org/data/2.5/onecall?exclude=hourly,minutely,alerts,current&lat="+cordinates[0]+"&lon="+cordinates[1]+"&appid="+apiKey
     const response = await axios.get(apiUrl);
     //console.log(esponse.data.daily.map(dateData => dateData.)
     setWeatherData(response.data.daily.map(dateData =>  ({
       "date" : new Date(dateData.dt*1000).toLocaleDateString("en-US"),
       "temp" : dateData.temp.day,
       "humidity": dateData.humidity
-    })
+      })
     ));
-    console.log(response.data);
+    console.log("Data Successfully Retrieved");
   }
 
   const filterData = (max,min,data) => {
@@ -39,7 +39,7 @@ function App() {
         filteredResults.push(dateData);
       }
     })
-
+    console.log("Data Successfully Filtered");
     return filteredResults; 
   }
   
@@ -51,25 +51,23 @@ function App() {
     if(e.target[1].value.length){
       setMax(parseInt(e.target[1].value));
     }
+    console.log("Limits Successfully Set");
+
   }
 
-
-
-  useEffect(pullWeatherData,[cook]);
-  //pullWeatherData()
+  useEffect(pullWeatherData,[cordinates]);
 
   return (
     <div className="App">
-      <form onSubmit={setLimits}>
-        <TextField id="standard-basic" label="Below" />
-        <TextField id="standard-basic" label="Above" />
-        <Button type="submit" style="contained">Get Data</Button>
-      </form>
-      
-      <FormControl
-            placeholder="Enter city"
-          />
-      {weatherData && <Table data={filterData(maxFilter,minFilter,weatherData)}/>}
+      <Grid container direction="row" justify="center" alignItems="center" >
+        <form onSubmit={setLimits}>
+          <TextField id="standard-basic" type="number" label="Below" />
+          <TextField id="standard-basic" type="number" label="Above" />
+          <Button type="submit" variant="contained">Get Data</Button>
+        </form>
+        {weatherData && <Table data={filterData(maxFilter,minFilter,weatherData)}/>}
+        <p>Cordinates: {cordinates[0]},{cordinates[1]}</p>
+      </Grid>
     </div>
   );
 }
--- src/util/WeatherService.js
+++
@@ -1,9 +0,0 @@
-const Forecast = () => {
-    function getForecast() {
-       // weather data fetch function will go here
-    }
-    return (
-        // JSX code will go here
-    )
- }
- export default Forecast;
