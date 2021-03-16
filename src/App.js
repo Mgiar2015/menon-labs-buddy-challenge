8554777c2b6a3fe696274c9e6f22fd25f778defd
Mgiar2015 <mgiar2015@gmail.com>
2021-03-16 16:00:07 -0400

    Added Table, feed in data from api into table

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React, { useState, useEffect } from 'react';
+import { Button, TextField, FormControl } from '@material-ui/core';
+import axios from 'axios';
+import Table from './components/Table';
 
-import Axios from 'axios';
+
 
 function App() {
   const apiKey = "7083cd16161bdb6686c87eb80fd31922"
-  const [userData, setUserData] = useState({
-    signedIn:true,
-    userProfile:{
-        id:1
-    }
-  });
+  const [weatherData,setWeatherData] = useState(null);
+  const [cords,setCords] = useState(null);
+
+  const pullWeatherData = async() => {
+    const lat = "33.441792";
+    const long = "-94.037689";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/onecall?exclude=hourly,minutely,alerts,current&lat="+lat+"&lon="+long+"&appid="+apiKey
+    const response = await axios.get(apiUrl);
+    //console.log(esponse.data.daily.map(dateData => dateData.)
+    setWeatherData(response.data.daily.map(dateData =>  ({
+      "date" : new Date(dateData.dt*1000).toLocaleDateString("en-US"),
+      "temp" : dateData.temp.day,
+      "humidity": dateData.humidity
+    })
+    ));
+    console.log(response.data);
+  }
+
+  useEffect(pullWeatherData,[cords]);
+  //pullWeatherData()
 
   return (
     <div className="App">
-
+      <FormControl>
+        <TextField id="standard-basic" label="Longitude" />
+        <TextField id="standard-basic" label="Lattitude" />
+        <Button>Get Data</Button>
+      </FormControl>
+      
+      <FormControl
+            placeholder="Enter city"
+          />
+      {weatherData && <Table data={weatherData}/>}
     </div>
   );
 }
---
+++ src/components/Table.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+export default function BasicTable(props) {
+
+  return (
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell align="right">Tempature</TableCell>
+            <TableCell align="right">Humidity</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {props.data.map((row) => (
+            <TableRow key={row.date}>
+              <TableCell component="th" scope="row">
+                {row.date}
+              </TableCell>
+              <TableCell align="right">{row.temp}</TableCell>
+              <TableCell align="right">{row.humidity}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
